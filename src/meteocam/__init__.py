c22b10b267fae9dc83ee90008d0601ffe5fd719b
Yoseba <joseangelaguilerapaez@gmail.com>
2026-09-15 04:13:04 +0200

    Crear paquete Python inicial de MeteoCam

---
+++ src/meteocam/__init__.py
@@ -0,0 +1,9 @@
+"""MeteoCam.
+
+Agente de escritorio para la gestión, monitorización, diagnóstico y control
+de cámaras IP del proyecto MeteoArchidona.
+"""
+
+__version__ = "0.1.0"
+
+# Fin archivo: src/meteocam/__init__.py
